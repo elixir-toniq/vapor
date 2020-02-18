5f995d0e4a87dec96dfeb12fd601bdc37dbc2a4d
Chris Keathley <c@keathley.io>
2020-02-18 12:23:01 -0500

    Allow users to specify groups of config providers

---
+++ lib/vapor/providers/group.ex
@@ -0,0 +1,35 @@
+defmodule Vapor.Provider.Group do
+  @moduledoc """
+  Allows users to group together bits of configuration. This allows users to
+  avoid duplication and avoids conflicts in common names such as "port" and
+  "host".
+
+  ## Example
+
+  ```elixir
+  providers = [
+    %Group{
+      name: :primary_db,
+      providers: [
+        %Env{bindings: [port: "PRIMARY_DB_PORT", host: "PRIMARY_DB_HOST"]},
+      ]
+    },
+    %Group{
+      name: :redis,
+      providers: [
+        %Env{bindings: [port: "REDIS_PORT", host: "REDIS_HOST"]},
+      ]
+    },
+  ]
+  ```
+  """
+  defstruct providers: [], name: nil
+
+  defimpl Vapor.Provider do
+    def load(%{providers: providers, name: name}) do
+      with {:ok, config} <- Vapor.Loader.load(providers) do
+        {:ok, Map.put(Map.new(), name, Keyword.new(config))}
+      end
+    end
+  end
+end
