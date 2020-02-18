d7a5abf9d4af04199b35425ad691655f89d7c0af
Chris Keathley <c@keathley.io>
2020-02-18 09:26:39 -0500

    Support defaults and required flags in env provider

--- lib/vapor/providers/env.ex
+++ lib/vapor/providers/env.ex
@@ -10,30 +10,59 @@ defmodule Vapor.Provider.Env do
 
   defstruct bindings: [], required: true
 
-  @type bindings :: Keyword.t(String.t())
-
   defimpl Vapor.Provider do
     def load(%{bindings: bindings, required: required}) do
       envs = System.get_env()
 
       bound_envs =
         bindings
-        |> Map.new(fn {key, env} -> {key, Map.get(envs, env, :missing)} end)
+        |> Enum.map(&normalize_binding/1)
+        |> Enum.map(&create_binding(&1, envs))
+        |> Enum.into(%{})
 
       missing =
         bound_envs
-        |> Enum.filter(fn {_, env} -> env == :missing end)
-        |> Enum.map(fn {k, :missing} -> Keyword.get(bindings, k) end)
+        |> Enum.filter(fn {_, data} -> data.val == :missing end)
+        |> Enum.map(fn {_k, data} -> data.env end)
 
       if required && Enum.any?(missing) do
         {:error, "ENV vars not set: #{Enum.join(missing, ", ")}"}
       else
         envs =
           bound_envs
-          |> Enum.reject(fn {_, env} -> env == :missing end)
+          |> Enum.reject(fn {_, data} -> data.val == :missing end)
+          |> Enum.map(fn {name, data} -> {name, data.val} end)
           |> Enum.into(%{})
+
         {:ok, envs}
       end
+    end
+
+    defp normalize_binding({name, variable}) do
+      {name, %{val: nil, env: variable, opts: default_opts()}}
+    end
+    defp normalize_binding({name, variable, opts}) do
+      {name, %{val: nil, env: variable, opts: Keyword.merge(default_opts(), opts)}}
+    end
+
+    defp create_binding({name, data}, envs) do
+      case envs[data.env] do
+        nil ->
+          val = data.opts[:default] || (if data.opts[:required], do: :missing, else: nil)
+          {name, %{data | val: val}}
+
+        env ->
+          # Call the map function which defaults to identity
+          {name, %{data | val: data.opts[:map].(env)}}
+      end
+    end
+
+    defp default_opts do
+      [
+        map: fn x -> x end,
+        default: nil,
+        required: true,
+      ]
     end
   end
 end
